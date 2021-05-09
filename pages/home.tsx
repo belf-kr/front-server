6023cfe11854705417ec6607aa9d545907d888fc
Kyungeun Park <63892989+parkgang@users.noreply.github.com>
2021-05-10 01:00:45 +0900

    v0.0.5 (#16)
    * add: 기능 추가1
    
    * add: 기능추가2
    
    * ingress endpoint 히트 이슈 (#6)
    
    * v0.0.0 (#4)
    
    * add: 기능 추가1
    
    * add: 기능추가2
    
    * add: home 이동
    
    * [ADD] 오늘 하루가 얼마나 완벽한지 알려주는 내용 추가 (#8)
    
    * [ADD] json 형식 반환값 추가
    
    테스트입니다
    
    * [ADD] 오늘 하루가 얼마나 완벽한지 알려주는 내용 추가
    
    * add: ok 추가
    
    Co-authored-by: Kyungeun <ruddms936@naver.com>
    
    * home 페이지에 텍스트 추가 (#12)
    
    * add: 텍스트 추가
    
    * add: 기능 추가2
    
    * 브랜치 변경에 따른 테스트 (#15)
    
    * add: 텍스트 추가
    
    * add: 기능 추가2
    
    * add: 테스트를 위한 버튼 추가
    
    * add: 기능 추가
    
    * add: 기능추가2
    
    Co-authored-by: JP3Pe <45834750+JP3Pe@users.noreply.github.com>

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,8 +4,6 @@ const HomePage: FC = () => {
   return (
     <div>
       <h1>HomePage</h1>
-      <p>Hi, What a wonderful day!</p>
-      <p>Ok</p>
       <p>기능 추가1</p>
       <p>기능 추가2</p>
     </div>
