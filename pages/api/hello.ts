4149f27a4d197bcbe387accc448feb2fb6cc7dd7
JP3Pe <45834750+JP3Pe@users.noreply.github.com>
2021-05-09 22:05:56 +0900

    [ADD] 오늘 하루가 얼마나 완벽한지 알려주는 내용 추가 (#8)
    * [ADD] json 형식 반환값 추가
    
    테스트입니다
    
    * [ADD] 오늘 하루가 얼마나 완벽한지 알려주는 내용 추가
    
    * add: ok 추가
    
    Co-authored-by: Kyungeun <ruddms936@naver.com>

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,6 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name2: "JaeKwang Kim" });
 };
 
 export default handler;
