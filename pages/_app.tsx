3f5c93b4d364485cf1ea23d828262b0818d28a1f
Leo <51093294+algo2000@users.noreply.github.com>
2021-05-13 22:11:40 +0900

    디렉토리 구조설정, recoil (#27)
    * init: 디렉토리 구조 변경
    
    * docs: 폴더 구조에 대한 설명 README.md에 추가
    
    * package: recoil 패키지 설치
    
    * docs: README.md 에 패키지 버전 관련 항목 추가 및 커밋 규칙 추가
    
    * up: 세미 콜론 제거
    
    * add: _app.tsx 파일 추가 및 RecoilRoot 사용
    
    * add: recoil 테스트를 위한 코드 작성
    
    * del: 남아있던 test script 삭
    fix: 남아있는 yarn to npm 으로 마이그레이션
    test: 포멧팅 검사
    
    * test: 포멧팅 검사를 위해 텍스트 추가
    
    * fix: push시 자동 포멧터 활성화
    
    * docs: history 반영
    
    * del: commit 으로 자동 포멧터 변경
    docs: 문서 반영
    
    * test: 포멧터 검사를 위해 ts 파일 확장자로 변경
    
    * docs: 히스토리 문서 반영
    
    * test: 포멧터를 강제로 트리거 시키기 위해 수정합니다.
    
    * style: prettier 인덴트
    
    * up: 이전 파일으로 원상복구
    
    Co-authored-by: Kyungeun <ruddms936@naver.com>

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </>
+  );
+}
