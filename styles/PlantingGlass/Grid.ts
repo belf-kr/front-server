b0b013ea34b92d1019906eff1dcd601f90f041fc
leo <algo2000@naver.com>
2021-05-15 03:35:08 +0900

    add: #23 잔디 심기 컴포넌트 스타일 추가

---
+++ styles/PlantingGlass/Grid.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(24, 10px);
+  grid-template-rows: 94px;
+  grid-gap: 4px;
+`;
+
+export default Grid;
---
+++ styles/PlantingGlass/index.ts
@@ -0,0 +1,15 @@
+import Card from "./Card";
+import DayItem from "./DayItem";
+import Grid from "./Grid";
+import WeekGrid from "./WeekGrid";
+import WeekItem from "./WeekItem";
+
+const PlantingGlass = {
+  Card: Card,
+  Grid: Grid,
+  WeekGrid: WeekGrid,
+  WeekItem: WeekItem,
+  DayItem: DayItem,
+};
+
+export default PlantingGlass;
