802e41f5e94234756d2f1adabcce254c0631f5e8
leo <algo2000@naver.com>
2021-05-14 14:06:29 +0900

    del: 테스트 코드 삭제

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Link from "next/link";
-import Display from "../components/Display";
-import H2Style from "../styles/H2Style";
 
 function AboutPage(): JSX.Element {
   return (
     <>
-      <H2Style>About</H2Style>
-      <Display />
       <Link href="/">
         <a>Go home</a>
       </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Link from "next/link";
-import Input from "../components/Input";
-import H2Style from "../styles/H2Style";
 import RepresentativeLayout from "../components/layouts/RepresentativeLayout";
 
 function IndexPage(): JSX.Element {
   return (
     <RepresentativeLayout title="Today" MainTitle={() => <a>test</a>}>
       <>
-        <H2Style highlight>Home</H2Style>
-        <Input />
         <p>
           <Link href="/about">
             <a>About</a>
