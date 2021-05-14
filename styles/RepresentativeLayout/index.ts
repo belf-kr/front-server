03045cd6c85e96a8f63e4cfcfe7bc9d57047dba3
leo <algo2000@naver.com>
2021-05-15 03:04:16 +0900

    add: #25 - 오늘 할일 페이지 레이아웃을 위한 대표 레이아웃에 공간 생성

--- styles/RepresentativeLayout/index.ts
+++ styles/RepresentativeLayout/index.ts
@@ -1,9 +1,11 @@
+import ContentsLayout from "./ContentsLayout";
 import Header from "./Header";
 import HeaderBackground from "./HeaderBackground";
 
 const RepresentativeLayout = {
   Header: Header,
   HeaderBackground: HeaderBackground,
+  ContentsLayout: ContentsLayout,
 };
 
 export default RepresentativeLayout;
