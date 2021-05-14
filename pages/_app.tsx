2e8378e34ba849fc74002cc5a1f36ffaae4241f5
leo <algo2000@naver.com>
2021-05-15 00:17:37 +0900

    add: 공용스타일 추가 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import GlobalStyle from "../styles/GlobalStyle";
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <RecoilRoot>
+        <GlobalStyle />
         <Component {...pageProps} />
       </RecoilRoot>
     </>
---
+++ styles/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    margin: 0;
+    padding: 0;
+    background-color: #FFF6EF;
+    font-family: "SF Mono", "Consolas", "Menlo", monospace;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    max-width: 390px;
+  }
+`;
+
+export default GlobalStyle;
