4adfed3ffa099cccb21de936465c4a0963fb2fea
leo <algo2000@naver.com>
2021-05-14 14:05:03 +0900

    add: #26 - Representative Layout 생성 및 테스트 작성

---
+++ styles/RepresentativeLayout/Header.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+  width: 100%;
+  margin: 0px 20px;
+`;
+
+export default Header;
---
+++ styles/RepresentativeLayout/index.ts
@@ -0,0 +1,7 @@
+import Header from "./Header";
+
+const RepresentativeLayout = {
+  Header: Header,
+};
+
+export default RepresentativeLayout;
