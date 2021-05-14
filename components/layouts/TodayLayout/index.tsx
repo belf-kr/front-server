7b2a2f221dde5a1db70eb0c970636ea4e3eb607e
leo <algo2000@naver.com>
2021-05-15 03:23:21 +0900

    add: #25 - 오늘 할일 페이지 레이아웃의 섹션별 스타일 지정

---
+++ components/layouts/TodayLayout/index.tsx
@@ -0,0 +1,12 @@
+import { default as S } from "../../../styles/TodayLayout";
+
+function TodayLayout(): JSX.Element {
+  return (
+    <>
+      <S.PlantingGlassSection></S.PlantingGlassSection>
+      <S.TodoListSection></S.TodoListSection>
+    </>
+  );
+}
+
+export default TodayLayout;
---
+++ styles/TodayLayout/index.ts
@@ -0,0 +1,9 @@
+import PlantingGlassSection from "./PlantingGlassSection";
+import TodoListSection from "./TodoListSection";
+
+const RepresentativeLayout = {
+  TodoListSection: TodoListSection,
+  PlantingGlassSection: PlantingGlassSection,
+};
+
+export default RepresentativeLayout;
