933b6736fb6301a767236ba3c3371e3275723167
leo <algo2000@naver.com>
2021-05-16 21:19:31 +0900

    add: #24 - 할일 목록 리스트 스타일 제작

---
+++ styles/TodayTodoItem/TodoItemCard.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const TodoItemCard = styled.div<{ left?: number }>`
+  box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
+  background-color: white;
+  border-radius: 10px;
+  padding: 10px;
+  box-sizing: border-box;
+  overflow: hidden;
+  transform: translateX(${props => props.left}px);
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+`;
+
+export default TodoItemCard;
