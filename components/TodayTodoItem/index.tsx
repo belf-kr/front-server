408157826772ca111956bd260bf02ce7cc1e0b7e
leo <algo2000@naver.com>
2021-05-20 22:57:46 +0900

    add: #32 svg 아이콘의 타입과 각각의 아이콘을 저장하는 static 폴더 생성

--- components/TodayTodoItem/index.tsx
+++ components/TodayTodoItem/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import useClick from "../../hooks/useClick";
+import bigCheck from "../../icons/bigCheck";
+import bigDelete from "../../icons/bigDelete";
 import { default as S } from "../../styles/TodayTodoItem";
 import { TodoItem } from "../../utils/type/TodoTodayItem/indes";
 
@@ -29,13 +31,13 @@ function TodayTodoItem({ todoItem }: props): JSX.Element {
       </S.TodoItemCard>
       <S.ButtonSection>
         <S.EventButton>
-          <S.Icon width={32} height={23} fill={"#29A4FF"}>
-            <path d="M9.79,22.23.75,13a2.63,2.63,0,0,1,0-3.68,2.53,2.53,0,0,1,3.57,0l0,0,7.42,7.56L27.64.76a2.51,2.51,0,0,1,3.56,0l.05,0a2.63,2.63,0,0,1,0,3.68L13.77,22.24a2.49,2.49,0,0,1-2,.75h-.19A2.5,2.5,0,0,1,9.79,22.23Z" />
+          <S.Icon width={bigCheck.width} height={bigCheck.height} fill={bigCheck.fill}>
+            <path d={bigCheck.d} />
           </S.Icon>
         </S.EventButton> 
         <S.EventButton>
-          <S.Icon width={23} height={23} fill={"#FF587E"}>
-            <path d="M18.67,22.26,11.5,15.09,4.33,22.26A2.54,2.54,0,1,1,.74,18.67h0L7.91,11.5.74,4.33A2.54,2.54,0,0,1,.74.74h0a2.54,2.54,0,0,1,3.59,0L11.5,7.91,18.67.74a2.54,2.54,0,0,1,3.59,0h0a2.54,2.54,0,0,1,0,3.59L15.09,11.5l7.17,7.17a2.54,2.54,0,0,1,0,3.59,2.55,2.55,0,0,1-3.59,0Z" />
+          <S.Icon width={bigDelete.width} height={bigDelete.height} fill={bigDelete.fill}>
+            <path d={bigDelete.d} />
           </S.Icon>
         </S.EventButton> 
       </S.ButtonSection>
