eb9e2124e34235ab7a047f2412ac89d2349504b7
leo <algo2000@naver.com>
2021-05-21 20:00:54 +0900

    #32 theme적용을 위한 코드 작성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import GlobalStyle from "../styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import lightTheme from "../theme/lightTheme";
 
 import { version } from "../package.json";
 
