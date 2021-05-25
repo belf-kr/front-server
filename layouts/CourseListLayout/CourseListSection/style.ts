cb66b5267f481df5dedcfca82ba7379a12da5b37
leo <algo2000@naver.com>
2021-05-25 23:54:11 +0900

    add: #42 코스리스트 컴포넌트가 들어가는 섹션 생성
    임시로 파란색상으로 영역을 표시

---
+++ layouts/CourseListLayout/CourseListSection/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const CourseListSection = styled.div`
+  max-width: 350px;
+  margin: auto;
+  margin-top: 30px;
+  overflow-x: visible;
+  z-index: -1;
+
+  background-color: blue;
+  height: 1000px;
+`;
+
+const style = {
+  CourseListSection: CourseListSection,
+};
+
+export default style;
