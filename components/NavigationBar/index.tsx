ec9557fc315b9b1f4af0af967672f7efb810110f
leo <algo2000@naver.com>
2021-05-31 00:51:26 +0900

    up: #33 - 네비게이션바 기능 추가

--- components/NavigationBar/index.tsx
+++ components/NavigationBar/index.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
 import { default as S } from "./style";
 
 import addCross from "../../assets/icons/addCross";
@@ -6,32 +9,73 @@ import course from "../../assets/icons/course";
 import board from "../../assets/icons/board";
 import user from "../../assets/icons/user";
 
-function NavigationBar(): JSX.Element {
+import { Icon } from "../../libs/type/Icon";
+
+type props = {
+  pageType: string;
+};
+
+type menuType = {
+  icon: Icon;
+  isActive: boolean;
+  pageType: string;
+  uri?: string;
+};
+
+const boolToColor = (isActive: boolean): string => {
+  if (isActive) {
+    return "#FF924A";
+  } else {
+    return "#D6D6D6";
+  }
+};
+
+function NavigationBar({ pageType }: props): JSX.Element {
+  const [menuList, setMenuList] = useState<menuType[]>([
+    {
+      icon: home,
+      isActive: true,
+      pageType: "Today",
+      uri: "/",
+    },
+    {
+      icon: course,
+      isActive: false,
+      pageType: "Course",
+      uri: "/course-list",
+    },
+    {
+      icon: board,
+      isActive: false,
+      pageType: "Board",
+    },
+    {
+      icon: user,
+      isActive: false,
+      pageType: "User",
+    },
+  ]);
+
+  useEffect(() => {
+    const index = menuList.findIndex((i) => i.pageType == pageType);
+    const newArr = menuList.map((e: menuType, i: number): menuType => ({ ...e, isActive: i == index }));
+    setMenuList(newArr);
+  }, []);
+
   return (
     <S.Frame>
       <S.Card>
         <S.MenuSection>
           <S.MenuIconSection>
-            <S.MenuIconArea>
-              <S.Icon {...home}>
-                <path d={home.d} />
-              </S.Icon>
-            </S.MenuIconArea>
-            <S.MenuIconArea>
-              <S.Icon {...course}>
-                <path d={course.d} />
-              </S.Icon>
-            </S.MenuIconArea>
-            <S.MenuIconArea>
-              <S.Icon {...board}>
-                <path d={board.d} />
-              </S.Icon>
-            </S.MenuIconArea>
-            <S.MenuIconArea>
-              <S.Icon {...user}>
-                <path d={user.d} />
-              </S.Icon>
-            </S.MenuIconArea>
+            {menuList.map((menu: menuType, index: number) => (
+              <Link href={menu.uri == undefined ? "" : menu.uri} key={"i_" + index}>
+                <S.MenuIconArea>
+                  <S.Icon {...menu.icon} fill={boolToColor(menu.isActive)}>
+                    <path d={menu.icon.d} />
+                  </S.Icon>
+                </S.MenuIconArea>
+              </Link>
+            ))}
           </S.MenuIconSection>
         </S.MenuSection>
         <S.ButtonSection>
