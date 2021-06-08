9abaac973e283bc88ff43e6820b03318fde7ab71
Leo <algo2000@naver.com>
2021-06-08 22:51:17 +0900

    add: 코스 추가 페이지 추가

--- pages/add/course/index.tsx
+++ pages/add/course/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import ColorGrid from "../../../components/ColorGrid";
+import Input from "../../../components/Input";
+
 import PageTitle from "../../../components/PageTitle";
 
+import AddLayout from "../../../layouts/AddLayout";
+import ColorsSection from "../../../layouts/AddLayout/ColorsSection";
 import RepresentativeLayout from "../../../layouts/RepresentativeLayout";
 import ContentsSection from "../../../layouts/RepresentativeLayout/ContentsSection";
 import HeaderSection from "../../../layouts/RepresentativeLayout/HeaderSection";
+import InputSection from "../../../layouts/AddLayout/InputSection";
+import TagInput from "../../../components/TagInput";
 
 function AddCoursePage(): JSX.Element {
   const pageTitle = "New Course";
@@ -13,7 +20,22 @@ function AddCoursePage(): JSX.Element {
       <HeaderSection>
         <PageTitle titleText={pageTitle} />
       </HeaderSection>
-      <ContentsSection isNavNone={true}></ContentsSection>
+      <ContentsSection isNavNone={true}>
+        <AddLayout>
+          <ColorsSection>
+            <ColorGrid />
+          </ColorsSection>
+          <InputSection>
+            <Input placeholder={"Title"} />
+          </InputSection>
+          <InputSection>
+            <Input placeholder={"Memo"} isMultiLine={true} />
+          </InputSection>
+          <InputSection>
+            <TagInput placeholder={"Tag"} />
+          </InputSection>
+        </AddLayout>
+      </ContentsSection>
     </RepresentativeLayout>
   );
 }
