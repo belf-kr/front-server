9605b7d2f1e42c8de33682023225098f9f5a87cc
Leo <algo2000@naver.com>
2021-06-08 22:53:28 +0900

    add: #56 - ColorGrid 컴포넌트 생성

---
+++ components/ColorGrid/Component.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { default as S } from "./style";
+
+type props = {
+  colors: string[];
+};
+
+function Component({ colors }: props): JSX.Element {
+  const [colorsObject, setColorsObject] = useState(
+    colors.map((element, index) => {
+      return { id: index, color: element, isChecked: index === 0 ? true : false };
+    })
+  );
+
+  function colorClick(i) {
+    setColorsObject(
+      colorsObject.map((value, index) => {
+        if (index === i) {
+          value.isChecked = true;
+        } else {
+          value.isChecked = false;
+        }
+        return value;
+      })
+    );
+  }
+
+  return (
+    <S.Grid>
+      {colorsObject.map((item, index) => (
+        <S.Item key={index} color={item.color} onClick={() => colorClick(index)}>
+          <S.ItemBorder isChecked={item.isChecked} />
+        </S.Item>
+      ))}
+    </S.Grid>
+  );
+}
+export default Component;
---
+++ components/ColorGrid/index.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+import getColors from "../../libs/axios/getColors";
+
+import Component from "./Component";
+
+function AllLikeCount(): JSX.Element {
+  const [component, setComponent] = useState<JSX.Element>(<>Loading...</>);
+
+  useEffect(() => {
+    getColors()
+      .then((res) => {
+        setComponent(<Component colors={res} />);
+      })
+      .catch(() => {
+        setComponent(<>Error...</>);
+      });
+  }, []);
+
+  return component;
+}
+
+export default AllLikeCount;
