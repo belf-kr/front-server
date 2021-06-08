f363ad8e0e827f633c0da528a84fb891f7b42776
leo <algo2000@naver.com>
2021-06-08 23:33:15 +0900

    up: new course 링크 추가

--- components/AddCourseButton/index.tsx
+++ components/AddCourseButton/index.tsx
@@ -1,7 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
 import { default as S } from "./style";
 
 function AddCourseButton(): JSX.Element {
-  return <S.Button>New Course</S.Button>;
+  return (
+    <Link href="/add/course">
+      <S.Button>New Course</S.Button>
+    </Link>
+  );
 }
 
 export default AddCourseButton;
