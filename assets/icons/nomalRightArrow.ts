81d259e61804483779ead534bdea8a51ae276cc7
Leo <algo2000@naver.com>
2021-06-11 13:15:17 +0900

    add: #53 - Detail 컴포넌트 제작

---
+++ assets/icons/nomalRightArrow.ts
@@ -0,0 +1,10 @@
+import { Icon } from "../../libs/type/Icon";
+
+const nomalRightArrow: Icon = {
+  d: "M0,12.94,5.89,7,0,1.06,1.06,0,8,7,7,8.06H7L1.05,14Z",
+  width: 8,
+  height: 14,
+  fill: "#747474",
+};
+
+export default nomalRightArrow;
