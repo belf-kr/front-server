0c748235963c227b98e4be271da3e96cd23fbe3c
Leo <algo2000@naver.com>
2021-06-12 10:21:45 +0900

    up: #48 - TodayTodoList mock server call 하도록 수정

--- libs/axios/getTodayTodoList.ts
+++ libs/axios/getTodayTodoList.ts
@@ -1,14 +1,15 @@
-// import axios from "axios";
+import axios from "axios";
 
 import { TodoItem } from "../type/TodoTodayItem";
 
-import todayTodoList from "../../data/todayTodoList.json";
+// import todayTodoList from "../../data/todayTodoList.json";
 
 async function getTodayTodoList(): Promise<TodoItem[]> {
   try {
-    // const res = await axios.get("url");
-    // const json: TodoItem[] = await res.data;
-    const json: TodoItem[] = todayTodoList.todo_list;
+    const res = await axios.get("https://api-gateway.qa.belf.xyz/todo/today-todos");
+    const json: TodoItem[] = await res.data.todo_list;
+
+    // const json: TodoItem[] = todayTodoList.todo_list;
 
     return json;
   } catch (e) {
