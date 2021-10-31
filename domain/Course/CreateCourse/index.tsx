6d637331998247b83fbb8ff4f832dd46d4e51cd0
leo <algo2000@naver.com>
2021-10-31 23:11:12 +0900

    up 코스 추가 기능(데이터 Validation X)

--- domain/Course/CreateCourse/index.tsx
+++ domain/Course/CreateCourse/index.tsx
@@ -1,23 +1,34 @@
-import React, { FormEvent } from "react";
+import React from "react";
+
+import { useRouter } from "next/router";
 
 import * as S from "./style";
 
-import TagInput from "../../Tag/TagInput";
-import Button from "../../../components/Button";
-import SelectCourseColor from "../SelectCourseColor";
 import useGetString from "../../../hooks/useGetString";
 import useOnChange from "../../../hooks/useOnChange";
+import useTagOnChange from "../../../hooks/useTagOnChange";
+
+import Button from "../../../components/Button";
+import SelectCourseColor from "../SelectCourseColor";
+import TagInput from "../../Tag/TagInput";
+import { postNewCourse } from "../../../libs/course";
 
 export default function CreateCourse(): JSX.Element {
+  const router = useRouter();
+
   const [color, setColor] = useGetString();
   const [title, setTitle] = useOnChange();
   const [explanation, setExplanation] = useOnChange();
+  const [tags, setTags] = useTagOnChange();
 
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
-    console.log(color);
-    console.log(title);
-    console.log(explanation);
+  const addCourse = async () => {
+    await postNewCourse({
+      title: title,
+      explanation: explanation,
+      color: color,
+      tags: tags,
+    });
+    router.back();
   };
 
   return (
@@ -25,31 +36,29 @@ export default function CreateCourse(): JSX.Element {
       <S.TitleBox>
         <S.Title>새로운 코스 추가</S.Title>
       </S.TitleBox>
-      <form onSubmit={handleSubmit}>
-        <S.Contents>
-          <S.SubTitleBox>
-            <S.SubTitle>코스 색상 선택</S.SubTitle>
-          </S.SubTitleBox>
-          <S.RadioColorsBox>
-            <SelectCourseColor colorOnChange={setColor} />
-          </S.RadioColorsBox>
-          <S.SubTitleBox>
-            <S.SubTitle>코스 제목</S.SubTitle>
-          </S.SubTitleBox>
-          <S.DefaultInput type="text" placeholder="입력해주세요" name="title" onChange={setTitle} />
-          <S.SubTitleBox>
-            <S.SubTitle>설명</S.SubTitle>
-          </S.SubTitleBox>
-          <S.DefaultTextArea placeholder="입력해주세요 (200자)" name="explanation" rows={6} maxLength={200} onChange={setExplanation} />
-          <S.SubTitleBox>
-            <S.SubTitle>태그</S.SubTitle>
-          </S.SubTitleBox>
-          <TagInput />
-          <S.SubmitButtonBox>
-            <Button type={"submit"} text={"코스 생성"} />
-          </S.SubmitButtonBox>
-        </S.Contents>
-      </form>
+      <S.Contents>
+        <S.SubTitleBox>
+          <S.SubTitle>코스 색상 선택</S.SubTitle>
+        </S.SubTitleBox>
+        <S.RadioColorsBox>
+          <SelectCourseColor colorOnChange={setColor} />
+        </S.RadioColorsBox>
+        <S.SubTitleBox>
+          <S.SubTitle>코스 제목</S.SubTitle>
+        </S.SubTitleBox>
+        <S.DefaultInput type="text" placeholder="입력해주세요" name="title" onChange={setTitle} />
+        <S.SubTitleBox>
+          <S.SubTitle>설명</S.SubTitle>
+        </S.SubTitleBox>
+        <S.DefaultTextArea placeholder="입력해주세요 (200자)" name="explanation" rows={6} maxLength={200} onChange={setExplanation} />
+        <S.SubTitleBox>
+          <S.SubTitle>태그</S.SubTitle>
+        </S.SubTitleBox>
+        <TagInput tagsOnChange={setTags} />
+        <S.SubmitButtonBox onClick={addCourse}>
+          <Button type={"submit"} text={"코스 생성"} />
+        </S.SubmitButtonBox>
+      </S.Contents>
     </>
   );
 }
