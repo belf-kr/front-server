be698ff1a23446dfbdefb6c3acc3c5507ec29f0e
leo <algo2000@naver.com>
2021-10-31 22:39:56 +0900

    up course Color 선택 컴포넌트로 뺌

--- domain/Course/CreateCourse/index.tsx
+++ domain/Course/CreateCourse/index.tsx
@@ -1,25 +1,23 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { FormEvent } from "react";
 
 import * as S from "./style";
 
-import { getColors } from "../../../libs/colors";
-
 import TagInput from "../../Tag/TagInput";
 import Button from "../../../components/Button";
+import SelectCourseColor from "../SelectCourseColor";
+import useGetString from "../../../hooks/useGetString";
+import useOnChange from "../../../hooks/useOnChange";
 
 export default function CreateCourse(): JSX.Element {
-  const [courseColors, setCourseColors] = useState<string[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      const res = await getColors();
-      setCourseColors(res);
-    })();
-    return () => setCourseColors([]);
-  }, []);
+  const [color, setColor] = useGetString();
+  const [title, setTitle] = useOnChange();
+  const [explanation, setExplanation] = useOnChange();
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
+    console.log(color);
+    console.log(title);
+    console.log(explanation);
   };
 
   return (
@@ -33,18 +31,16 @@ export default function CreateCourse(): JSX.Element {
             <S.SubTitle>코스 색상 선택</S.SubTitle>
           </S.SubTitleBox>
           <S.RadioColorsBox>
-            {courseColors.map((color, index) => {
-              return <S.ColorInput type="radio" color={color} value={color} name="color" key={index} defaultChecked={index === 0 ? true : false} />;
-            })}
+            <SelectCourseColor colorOnChange={setColor} />
           </S.RadioColorsBox>
           <S.SubTitleBox>
             <S.SubTitle>코스 제목</S.SubTitle>
           </S.SubTitleBox>
-          <S.DefaultInput type="text" placeholder="입력해주세요" name="title" />
+          <S.DefaultInput type="text" placeholder="입력해주세요" name="title" onChange={setTitle} />
           <S.SubTitleBox>
             <S.SubTitle>설명</S.SubTitle>
           </S.SubTitleBox>
-          <S.DefaultTextArea placeholder="입력해주세요 (200자)" name="explanation" rows={6} maxLength={200} />
+          <S.DefaultTextArea placeholder="입력해주세요 (200자)" name="explanation" rows={6} maxLength={200} onChange={setExplanation} />
           <S.SubTitleBox>
             <S.SubTitle>태그</S.SubTitle>
           </S.SubTitleBox>
