09fa8d5980c1d4808b67f9eccd3e3019bff31498
leo <algo2000@naver.com>
2021-10-31 21:28:59 +0900

    add (todo 추가) api Req/Res 추가

--- libs/todo/index.ts
+++ libs/todo/index.ts
@@ -6,3 +6,7 @@ export async function getTodayTodos(): Promise<TodoItem[]> {
   const { data } = await client.get<TodoItem[]>(`/work-todos`);
   return data;
 }
+
+export async function postNewTodo(todo: TodoItem): Promise<void> {
+  await client.post<TodoItem>(`/work-todos`, todo);
+}
