ff0cb720ff372aa381b4ba85788ac81f3a7fd07c
leo <algo2000@naver.com>
2021-11-01 01:14:59 +0900

    up (할일 추가) 추가

--- domain/Todo/CreateTodo/index.tsx
+++ domain/Todo/CreateTodo/index.tsx
@@ -1,14 +1,34 @@
-import React, { FormEvent } from "react";
+import React from "react";
+
+import { useRouter } from "next/router";
 
 import * as S from "./style";
+
+import { postNewTodo } from "../../../libs/todo";
 
 import Button from "../../../components/Button";
 import SelectCourse from "../SelectCourse";
 import SelectDayOfWeek from "../SelectDayOfWeek";
 
+import useOnChange from "../../../hooks/useOnChange";
+import useCourseOnChange from "../../../hooks/useCourseOnChange";
+
 export default function CreateTodo(): JSX.Element {
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
+  const router = useRouter();
+
+  const [title, setTitle] = useOnChange();
+  const [explanation, setExplanation] = useOnChange();
+  const [course, setCourse] = useCourseOnChange();
+
+  const addTodo = async () => {
+    await postNewTodo({
+      courseId: course.id,
+      title: title,
+      explanation: explanation,
+      recurringCycleDate: 1,
+      passedDay: 1,
+    });
+    router.back();
   };
 
   return (
@@ -16,25 +36,27 @@ export default function CreateTodo(): JSX.Element {
       <S.TitleBox>
         <S.Title>새로운 할일 추가</S.Title>
       </S.TitleBox>
-      <form onSubmit={handleSubmit}>
-        <S.Contents>
-          <S.SubTitleBox>
-            <S.SubTitle>할일 제목</S.SubTitle>
-          </S.SubTitleBox>
-          <S.DefaultInput type="text" placeholder="입력해주세요" name="title" />
-          <S.SubTitleBox>
-            <S.SubTitle>코스 선택</S.SubTitle>
-          </S.SubTitleBox>
-          <SelectCourse />
-          <S.SubTitleBox>
-            <S.SubTitle>옵션</S.SubTitle>
-          </S.SubTitleBox>
-          <SelectDayOfWeek />
-          <S.SubmitButtonBox>
-            <Button type={"submit"} text={"할일 생성"} />
-          </S.SubmitButtonBox>
-        </S.Contents>
-      </form>
+      <S.Contents>
+        <S.SubTitleBox>
+          <S.SubTitle>할일 제목</S.SubTitle>
+        </S.SubTitleBox>
+        <S.DefaultInput type="text" placeholder="입력해주세요" name="title" onChange={setTitle} />
+        <S.SubTitleBox>
+          <S.SubTitle>설명</S.SubTitle>
+        </S.SubTitleBox>
+        <S.DefaultTextArea placeholder="입력해주세요 (200자)" name="explanation" rows={6} maxLength={200} onChange={setExplanation} />
+        <S.SubTitleBox>
+          <S.SubTitle>코스 선택</S.SubTitle>
+        </S.SubTitleBox>
+        <SelectCourse courseOnChange={setCourse} />
+        <S.SubTitleBox>
+          <S.SubTitle>옵션</S.SubTitle>
+        </S.SubTitleBox>
+        <SelectDayOfWeek />
+        <S.SubmitButtonBox onClick={addTodo}>
+          <Button type={"submit"} text={"할일 생성"} />
+        </S.SubmitButtonBox>
+      </S.Contents>
     </>
   );
 }
