9873cdb974e71517a4957462632db002ac959861
Leo <51093294+algo2000@users.noreply.github.com>
2021-11-20 02:56:00 +0900

    기존 API Req/Res 인증 서버 연동 & useEffect 언마운트 경고 해결 (#88)
    * up course 생성 인증 추가
    
    * up todo 생 인증 추가
    
    * fix unmount 경고 해결
    
    * fix 에러 텍스트 변경 "postNewTodo() 에러"

--- libs/course/index.ts
+++ libs/course/index.ts
@@ -1,9 +1,32 @@
 import { apiClient } from "../api-client";
-
 import { CourseItem } from "../../types/components-type/course";
+import { getLocalStorageAccessToken, TokenRefresh } from "../oauth";
+import axios from "axios";
 
 export async function postNewCourse(course: CourseItem): Promise<void> {
-  await apiClient.post(`/courses`, course);
+  async function work() {
+    const accessToken = getLocalStorageAccessToken();
+    await apiClient.post(`/courses`, course, {
+      headers: {
+        Authorization: `${accessToken}`,
+      },
+    });
+  }
+  try {
+    return await work();
+  } catch (error) {
+    if (error instanceof Error) {
+      if (axios.isAxiosError(error)) {
+        switch (error.response?.status) {
+          case 401:
+            // 재시도: 리프레쉬 토큰으로 엑세스 토큰을 다시 발급
+            await TokenRefresh();
+            return await work();
+        }
+      }
+    }
+    throw new Error("postNewCourse() 에러");
+  }
 }
 
 export async function getCourses(): Promise<CourseItem[]> {
