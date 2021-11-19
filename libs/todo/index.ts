92ff035be73eb5b717f661369b4adad989dee9ee
leo <algo2000@naver.com>
2021-11-20 02:39:55 +0900

    up todo 생 인증 추가

--- libs/todo/index.ts
+++ libs/todo/index.ts
@@ -1,6 +1,8 @@
 import { apiClient } from "../api-client";
 
 import { TodoItem } from "../../types/components-type/todo";
+import { getLocalStorageAccessToken, TokenRefresh } from "../oauth";
+import axios from "axios";
 
 export async function getTodayTodos(): Promise<TodoItem[]> {
   const { data } = await apiClient.get<TodoItem[]>(`/work-todos`);
@@ -8,5 +10,27 @@ export async function getTodayTodos(): Promise<TodoItem[]> {
 }
 
 export async function postNewTodo(todo: TodoItem): Promise<void> {
-  await apiClient.post<TodoItem>(`/work-todos`, todo);
+  async function work() {
+    const accessToken = getLocalStorageAccessToken();
+    await apiClient.post<TodoItem>(`/work-todos`, todo, {
+      headers: {
+        Authorization: `${accessToken}`,
+      },
+    });
+  }
+  try {
+    return await work();
+  } catch (error) {
+    if (error instanceof Error) {
+      if (axios.isAxiosError(error)) {
+        switch (error.response?.status) {
+          case 401:
+            // 재시도: 리프레쉬 토큰으로 엑세스 토큰을 다시 발급
+            await TokenRefresh();
+            return await work();
+        }
+      }
+    }
+    throw new Error("postNewCourse() 에러");
+  }
 }
