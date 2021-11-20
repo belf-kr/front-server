f81a1eed51fd4bfb2cd81e248770def189811332
leo <algo2000@naver.com>
2021-11-20 10:41:42 +0900

    up course 리스트를 Query string userId 로 가져올 수 있도록 변경

--- libs/course/index.ts
+++ libs/course/index.ts
@@ -29,8 +29,9 @@ export async function postNewCourse(course: CourseItem): Promise<void> {
   }
 }
 
-export async function getCourses(): Promise<CourseItem[]> {
-  const { data } = await apiClient.get<CourseItem[]>(`/courses`);
+export async function getCourses(userId?: number): Promise<CourseItem[]> {
+  const queryString = [userId && `userId=${userId}`];
+  const { data } = await apiClient.get<CourseItem[]>(`/courses?` + queryString.join("&"));
   return data;
 }
 
