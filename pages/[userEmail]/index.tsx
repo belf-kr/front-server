91c769c77228bdb8c6064541a14e60e90c4b8bdc
leo <algo2000@naver.com>
2021-12-11 02:13:03 +0900

    fix : 잔디 심기 구현 불가로 인한 임시 컴포넌트 제거 픽스

--- pages/[userEmail]/index.tsx
+++ pages/[userEmail]/index.tsx
@@ -7,7 +7,6 @@ import UserContentsLayout from "../../layouts/UserContentsLayout";
 import UserProfile from "../../domain/UserPage/UserProfile";
 import UserPageNavigate from "../../domain/UserPage/UserPageNavigate";
 import CoursePinList from "../../domain/Course/CoursePinList";
-import PlantingGlass from "../../domain/Status/PlantingGlass";
 import TodayTodoList from "../../domain/Todo/TodayTodoList";
 import CourseList from "../../domain/Course/CourseList";
 
@@ -26,7 +25,6 @@ const getTabComponent = (key: string) => {
       return (
         <>
           <CoursePinList />
-          <PlantingGlass />
           <TodayTodoList />
         </>
       );
