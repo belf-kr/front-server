5b0dfb52bbec27d4fda2fc33ca0a2eca9f9cb9b2
leo <algo2000@naver.com>
2021-12-13 02:24:39 +0900

    up : 코스 디테일 페이지 이동 가능하도록 변경

--- domain/Course/SearchCourseItem/index.tsx
+++ domain/Course/SearchCourseItem/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import * as S from "./style";
 
+import Link from "next/link";
+
 import { CourseItem as CourseItemType } from "../../../types/components-type/course";
 
 import TagList from "../../Tag/TagList";
@@ -12,19 +14,21 @@ type props = {
 
 export default function SearchCourseItem({ courseItem }: props): JSX.Element {
   return (
-    <S.Card>
-      <S.Color backgroundColor={courseItem.color} />
-      <S.InfoBox>
-        <S.RowBox>
-          <S.Title>{courseItem.title}</S.Title>
-        </S.RowBox>
-        <S.RowBox>
-          <S.Explanation>{courseItem.explanation}</S.Explanation>
-        </S.RowBox>
-        <S.RowBox>
-          <TagList tagList={courseItem.tags} />
-        </S.RowBox>
-      </S.InfoBox>
-    </S.Card>
+    <Link href={`/${courseItem.userEmail}/${courseItem.id}`}>
+      <S.Card>
+        <S.Color backgroundColor={courseItem.color} />
+        <S.InfoBox>
+          <S.RowBox>
+            <S.Title>{courseItem.title}</S.Title>
+          </S.RowBox>
+          <S.RowBox>
+            <S.Explanation>{courseItem.explanation}</S.Explanation>
+          </S.RowBox>
+          <S.RowBox>
+            <TagList tagList={courseItem.tags} />
+          </S.RowBox>
+        </S.InfoBox>
+      </S.Card>
+    </Link>
   );
 }
