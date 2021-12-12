a9f77bb4177803869c2d4ad0b084f388cef52498
박경은 <ruddms936@naver.com>
2021-12-13 00:24:13 +0900

    fix: 권한이 없는 사용자는 투두 작성페이지로 이동하지 못하도록 수정

--- domain/Todo/TodayTodoItem/index.tsx
+++ domain/Todo/TodayTodoItem/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import * as S from "./style";
 
 import { TodoItem } from "../../../types/components-type/todo";
@@ -7,7 +7,6 @@ import { useRecoilValue } from "recoil";
 import { isPermissionState, queryStringUserState } from "../../../states/app";
 import Kebab from "../../../components/Kebab";
 import { MenuItemType } from "../../../types/components-type/kebab";
-import router from "next/router";
 import { deleteTodo } from "../../../libs/todo";
 
 type props = {
@@ -19,11 +18,19 @@ export default function TodayTodoItem({ todoItem, isLastItem }: props): JSX.Elem
   const queryStringUser = useRecoilValue(queryStringUserState);
   const isPermission = useRecoilValue(isPermissionState);
 
+  const router = useRouter();
+
   function handleClickMenuItem(item: TodoItem) {
     (async () => {
       await deleteTodo(item.id);
       router.reload();
     })();
+  }
+
+  function handleWriteTodo() {
+    if (isPermission) {
+      router.push(`/${queryStringUser.email}/${todoItem.courseId}/todo/${todoItem.id}/write`);
+    }
   }
 
   const menuItems: MenuItemType[] = [
@@ -36,26 +43,24 @@ export default function TodayTodoItem({ todoItem, isLastItem }: props): JSX.Elem
   ];
 
   return (
-    <Link href={`/${queryStringUser.email}/${todoItem.courseId}/todo/${todoItem.id}/write`} passHref={true}>
-      <S.TodoItemBox>
-        <S.Color backgroundColor={todoItem.color} />
-        <S.InfoBox>
-          <S.TextBox>
-            <S.TodoText>{todoItem.title}</S.TodoText>
-          </S.TextBox>
-          <S.TextBox>
-            <S.CourseText>{todoItem.courseTitle}</S.CourseText>
-          </S.TextBox>
-          <S.KebabBox>
-            {isPermission && (
-              <S.KebabBox>
-                <Kebab menuItems={menuItems} />
-              </S.KebabBox>
-            )}
-          </S.KebabBox>
-          <S.BorderBox isBorder={!isLastItem} />
-        </S.InfoBox>
-      </S.TodoItemBox>
-    </Link>
+    <S.TodoItemBox onClick={handleWriteTodo}>
+      <S.Color backgroundColor={todoItem.color} />
+      <S.InfoBox>
+        <S.TextBox>
+          <S.TodoText>{todoItem.title}</S.TodoText>
+        </S.TextBox>
+        <S.TextBox>
+          <S.CourseText>{todoItem.courseTitle}</S.CourseText>
+        </S.TextBox>
+        <S.KebabBox>
+          {isPermission && (
+            <S.KebabBox>
+              <Kebab menuItems={menuItems} />
+            </S.KebabBox>
+          )}
+        </S.KebabBox>
+        <S.BorderBox isBorder={!isLastItem} />
+      </S.InfoBox>
+    </S.TodoItemBox>
   );
 }
