0f4a042ee001371d3b6317ebe2dcf435c4dfa7a4
Kyungeun Park <63892989+parkgang@users.noreply.github.com>
2021-12-13 14:07:15 +0900

    사용자 프로필 사진 업로드, 회원 탈퇴 기능 추가 (#104)
    * up: ui 퀄리티 개선
    
    * up: ui 깨지는거 수정
    
    * add: 사용자 사진 업로드
    
    * add: 이미지 삭제 기능 연동 완료
    
    * add: 회원 탈되를 위한 컴펀창 그리기
    
    * add: 회원탈퇴 기능 완료
    
    * add: header bar 개선
    
    * add: 이미지 업로드 및 삭제시 로더 추가

--- domain/Setting/SettingMaster/index.tsx
+++ domain/Setting/SettingMaster/index.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useRecoilState } from "recoil";
 import Button from "../../../components/Button";
+import Loading from "../../../components/Loading";
+import { UserRemove, UsersAvatarRemove, UsersAvatarUpload } from "../../../libs/oauth";
 import { loginUserState } from "../../../states/app";
 import { imageDefault } from "../../UserPage/UserProfile";
 import EditButton from "../EditButton";
@@ -8,26 +11,86 @@ import EditButton from "../EditButton";
 import * as S from "./style";
 
 export default function SettingMaster(): JSX.Element {
-  const loginUser = useRecoilValue(loginUserState);
+  const [loginUser, setLoginUser] = useRecoilState(loginUserState);
+
+  const [isImgLoading, setIsImgLoading] = useState<boolean>();
+
+  const router = useRouter();
+
+  async function loadAvatar(event: any) {
+    try {
+      setIsImgLoading(true);
+      const target = event.target as HTMLInputElement;
+      const file = target.files[0];
+
+      // 낙관론적 업데이트
+      setLoginUser((prev) => {
+        return {
+          ...prev,
+          avatarImage: URL.createObjectURL(file),
+        };
+      });
+
+      await UsersAvatarUpload(file);
+      setIsImgLoading(false);
+    } catch (error) {
+      alert("사용자 사진 업로드 중 에러: " + error);
+    }
+  }
+
+  async function removeAvatar() {
+    try {
+      setIsImgLoading(true);
+      // 낙관론적 업데이트
+      setLoginUser((prev) => {
+        return {
+          ...prev,
+          avatarImage: imageDefault,
+        };
+      });
+
+      await UsersAvatarRemove();
+      setIsImgLoading(false);
+    } catch (error) {
+      alert("사용자 사진 삭제 중 에러: " + error);
+    }
+  }
+
+  async function removeUser() {
+    try {
+      await UserRemove();
+      router.replace("/");
+    } catch (error) {
+      alert("사용자 삭제 중 에러: " + error);
+    }
+  }
 
   return (
     <S.Box>
       <S.UserInfoBox>
         <S.UserImageBox>
-          <img src={loginUser.avatarImage ? loginUser.avatarImage : imageDefault} />
+          {isImgLoading ? (
+            <>
+              <Loading width="140px" height="140px" />
+            </>
+          ) : (
+            <img src={loginUser.avatarImage ? loginUser.avatarImage : imageDefault} />
+          )}
           <S.ImageEditButtonBox>
             <EditButton
               menuItems={[
                 {
                   showText: "이미지 업로드",
-                  onClick: () => {
-                    console.log("이미지 업로드 이벤트");
+                  onClick() {
+                    const image = document.querySelector("#chooseFile");
+                    const ev = new MouseEvent("click", { bubbles: true });
+                    image.dispatchEvent(ev);
                   },
                 },
                 {
                   showText: "이미지 삭제",
-                  onClick: () => {
-                    console.log("이미지 삭제 이벤트");
+                  onClick() {
+                    removeAvatar();
                   },
                 },
               ]}
@@ -42,18 +105,21 @@ export default function SettingMaster(): JSX.Element {
       <S.Line />
       <S.SettingOptionListBox>
         <S.SettingOptionBox>
-          <S.OptionTitleText>회원탈퇴</S.OptionTitleText>
           <S.OptionButtonBox>
             <Button
-              text="회원탈퇴"
+              text="회원 탈퇴"
               bg="#FF5446"
               onClick={() => {
-                console.log("회원탈퇴 이벤트");
+                const result = confirm("회원 탈퇴를 진행하시겠습니까? 탈퇴 후 데이터는 복구할 수 없습니다.");
+                if (result) {
+                  removeUser();
+                }
               }}
             />
           </S.OptionButtonBox>
         </S.SettingOptionBox>
       </S.SettingOptionListBox>
+      <input type="file" id="chooseFile" name="chooseFile" accept="image/*" style={{ display: "none" }} onChange={loadAvatar} />
     </S.Box>
   );
 }
