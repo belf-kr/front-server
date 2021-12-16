01a1100e8e16e193eacaee0c7814c457893007fb
박경은 <ruddms936@naver.com>
2021-12-16 20:19:52 +0900

    up: 탈퇴시 모든 사용자 데이터 날리도록

--- libs/todo/index.ts
+++ libs/todo/index.ts
@@ -72,3 +72,29 @@ export async function deleteTodo(id: number): Promise<void> {
     throw new Error("deleteTodo() 에러");
   }
 }
+
+export async function deleteUserTodoData(): Promise<void> {
+  async function work() {
+    const accessToken = getLocalStorageAccessToken();
+    await apiClient.delete(`/users`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  }
+  try {
+    return await work();
+  } catch (error) {
+    if (error instanceof Error) {
+      if (axios.isAxiosError(error)) {
+        switch (error.response?.status) {
+          case 401:
+            // 재시도: 리프레쉬 토큰으로 엑세스 토큰을 다시 발급
+            await TokenRefresh();
+            return await work();
+        }
+      }
+    }
+    throw new Error("deleteUserTodoData() 에러");
+  }
+}
