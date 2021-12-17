0c85b74d0c5b21ca846e01d7871f6d7ce7696c0e
박경은 <ruddms936@naver.com>
2021-12-18 06:01:23 +0900

    up: input 크기 변경 및  placeholder 내용 변경

--- domain/SearchInput/style.ts
+++ domain/SearchInput/style.ts
@@ -29,7 +29,7 @@ const Input = styled.input`
   color: ${({ theme }) => theme.fontColor.sub};
   padding: 5px 10px 5px 0px;
   flex: 1;
-  width: 100%;
+  width: 15rem;
 
   @media only screen and (max-width: 850px) {
     padding: 5px 10px;
